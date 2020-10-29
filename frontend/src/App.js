ef9f33c0cbf77c3f59b19ec37964a4b00c7f20a0
thongvm316 <vominhthong1603@gmail.com>
2020-10-29 17:07:12 +0700

    20201029

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screents/HomeScreen'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Header/>
+    <main className="py-3">
+      <Container>
+        <HomeScreen/>
+      </Container>
+    </main>
+    <Footer/>
+    </>
   );
 }
 
