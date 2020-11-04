fcb62cf68beec800fc2d15dd099c6c21cd17160b
thongvm316 <vominhthong1603@gmail.com>
2020-11-04 15:01:35 +0700

    20201104

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
-// const express = require('express');
 import express from 'express'
 import dotenv from 'dotenv'
-// const dotenv = require('dotenv');
-// const products = require('./data/products');
+import connectDB from './config/db.js'
 import products from './data/products.js'
+import colors from 'colors'
 
-dotenv.config()
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -24,4 +25,4 @@ app.get('/api/products/:id', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
--- frontend/todo.md
+++ frontend/todo.md
@@ -2,4 +2,6 @@
 
 2. Serving & Fetching Data From Express
 
-3. 
+3. Getting Start with MongoDB
+
+test reviewSchema in model
